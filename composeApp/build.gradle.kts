0c39aa84055cd656e22b165964c87dc4ccb0416a
Sendiko <sarangtawon897@gmail.com>
2025-11-05 16:56:44 +0700

    chore: added dependency configurations for Koin, Ktor, DataStore, and Serialization; updated Compose and library versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,19 +29,52 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+
+            /* Koin Android */
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
+
+            /* Ktor OkHttp */
+            implementation(libs.ktor.client.okhttp)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.navigation.compose)
+            implementation(libs.kotlinx.datetime)
+
+            /* DataStore */
+            api(libs.datastore)
+            api(libs.datastore.preferences)
+
+            /* Serialization Json */
+            implementation(libs.kotlinx.serialization.json)
+
+            /* Koin */
+            implementation(libs.koin.compose.viewmodel)
+            api(libs.koin.core)
+
+            /* Ktor */
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.logging)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.client.auth)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+        iosMain.dependencies {
+            /* Ktor Darwin */
+            implementation(libs.ktor.client.darwin)
         }
     }
 }
